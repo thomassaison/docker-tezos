de8242c910b4e0826518083095d186e6d35b4ccd
Thomas Saison <thomas.saison@octo.com>
2022-05-05 15:44:44 +0200

    Setup entrypoint

--- docker-tezos-node/entrypoint.sh
+++ docker-tezos-node/entrypoint.sh
@@ -1,2 +1,9 @@
 #! /bin/sh
 
+NETWORK_VERSION="ithacanet"
+DATA_DIR="/tezos-$NETWORK_VERSION"
+
+mkir -p $DATA_DIR
+tezos-node config init --data-dir $DATA_DIR --network $NETWORK_VERSION
+tezos-node identity generate --data-dir $DATA_DIR
+tezos-node run --rpc-addr 127.0.0.1 --data-dir $DATA_DIR
