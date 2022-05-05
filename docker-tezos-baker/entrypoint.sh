bd81322a17149eab91807c1f7a27e2d9743826a9
Thomas Saison <thomas.saison@octo.com>
2022-05-05 17:18:24 +0200

    [Baker] setup node endpoint

--- docker-tezos-baker/entrypoint.sh
+++ docker-tezos-baker/entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 NETWORK_VERSION="ithacanet"
 DATA_DIR="/tezos-node-$NETWORK_VERSION"
 
+if [ -z "$NODE_ENDPOINT" ]; then NODE_ENDPOINT="http://localhost:8732" ; fi
+
 tezos-baker run with local node $DATA_DIR
