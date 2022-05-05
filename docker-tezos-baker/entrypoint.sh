3c3ff14ac9a8b2ef3062209fa1b11f404e22e0e6
Thomas Saison <thomas.saison@octo.com>
2022-05-05 17:21:18 +0200

    Echo

--- docker-tezos-baker/entrypoint.sh
+++ docker-tezos-baker/entrypoint.sh
@@ -5,5 +5,5 @@ NETWORK_VERSION="ithacanet"
 DATA_DIR="/tezos-node-$NETWORK_VERSION"
 
 if [ -z "$NODE_ENDPOINT" ]; then NODE_ENDPOINT="http://localhost:8732" ; fi
-
+echo "NODE_ENDPOINT=$NODE_ENDPOINT"
 tezos-baker run with local node $DATA_DIR
