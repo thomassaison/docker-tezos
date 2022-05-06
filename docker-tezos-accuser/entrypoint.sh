4cbc82d07ada09d84f958525883d1fe86725cc9e
Thomas Saison <thomas.saison@octo.com>
2022-05-06 17:18:03 +0200

    Change accuser entrypoint

--- docker-tezos-accuser/entrypoint.sh
+++ docker-tezos-accuser/entrypoint.sh
@@ -1,18 +1,4 @@
 #! /bin/sh
 set -e
 
-NETWORK_VERSION="ithacanet"
-DATA_DIR="/tezos-$NETWORK_VERSION"
-
-# mkdir -p $DATA_DIR
-# tezos-node config init --data-dir $DATA_DIR --network $NETWORK_VERSION
-# tezos-node identity generate --data-dir $DATA_DIR
-
-# if [ ! -z "$SNAPSHOT" ]; 
-# then 
-#     wget --directory-prefix=/ "$SNAPSHOT"
-#     SNAPSHOT_NAME=$( echo $SNAPSHOT | sed "s/https:\/\/snapshots-tezos.giganode.io\/snapshots\/\(.*\)/\1/" )
-#     tezos-node snapshot import /$SNAPSHOT_NAME --data-dir $DATA_DIR
-# fi
-
-# tezos-node run --rpc-addr 127.0.0.1 --data-dir $DATA_DIR
+tezos-accuser run
