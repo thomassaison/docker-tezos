696e60f82568e562adb4b2cd2faf9620b858489c
Thomas Saison <thomas.saison@octo.com>
2022-05-06 11:09:59 +0200

    More log

--- docker-tezos-node/entrypoint.sh
+++ docker-tezos-node/entrypoint.sh
@@ -9,12 +9,16 @@ if [ ! -f "$DATA_DIR/config.json" ];
 then
     echo "Init config..."
     tezos-node config init --data-dir $DATA_DIR --network $NETWORK_VERSION
+else
+    echo "Initialization already did"
 fi
 
 if [ ! -f "$DATA_DIR/identity.json" ];
 then
     echo "Generating identity..."
     tezos-node identity generate --data-dir $DATA_DIR
+else
+    echo "Identity already generated"
 fi
 
 if [ ! -z "$SNAPSHOT" ]; 
@@ -23,6 +27,8 @@ then
     wget --directory-prefix=/ "$SNAPSHOT"
     SNAPSHOT_NAME=$( echo $SNAPSHOT | sed "s/https:\/\/snapshots-tezos.giganode.io\/snapshots\/\(.*\)/\1/" )
     tezos-node snapshot import /$SNAPSHOT_NAME --data-dir $DATA_DIR
+else
+    echo "No snapshot prodided"
 fi
 
 echo "Running..."
