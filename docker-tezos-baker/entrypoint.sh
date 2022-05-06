022c73d47cc6f22138ffc615230befd854baac78
Thomas Saison <thomas.saison@octo.com>
2022-05-06 11:47:21 +0200

    Change baker

--- docker-tezos-baker/entrypoint.sh
+++ docker-tezos-baker/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 NETWORK_VERSION="ithacanet"
 DATA_DIR="/tezos-node-$NETWORK_VERSION"
 
-if [ -z "$NODE_ENDPOINT" ]; then NODE_ENDPOINT="http://localhost:8732" ; fi
+if [ -z "$NODE_ENDPOINT" ]; then NODE_ENDPOINT="http://node:8732" ; fi
 echo "NODE_ENDPOINT=$NODE_ENDPOINT"
 tezos-baker run with local node $DATA_DIR
